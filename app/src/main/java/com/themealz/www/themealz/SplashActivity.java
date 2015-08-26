62003e45c4f604769d7998362f453df2185d86b7
Eli Sagy <sagyeli@gmail.com>
2015-08-26 04:01:40 +0300

    Splash screen

---
+++ app/src/main/java/com/themealz/www/themealz/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.themealz.www.themealz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 5;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
